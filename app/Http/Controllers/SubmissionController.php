438413a7eb883d79ec064fa120e7d19cf4489e91
Kishan Nath <kishansnath93@gmail.com>
2024-06-08 19:25:12 +0530

    fixed submission save queue job

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SubmissionCreateRequest;
 use App\Jobs\ProcessSubmission;
-use App\Models\Submission;
 
 class SubmissionController extends Controller
 {
     public function store(SubmissionCreateRequest $request)
     {
-        $submission = new Submission();
-        $submission->fill($request->validated());
 
-        ProcessSubmission::dispatch($submission);
+        ProcessSubmission::dispatch($request->validated());
 
         return response()->json([
             'message' => 'Submission queued for processing',
--- app/Jobs/ProcessSubmission.php
+++ app/Jobs/ProcessSubmission.php
@@ -9,20 +9,17 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-/**
- * @method static void dispatch(Submission $submission)
- */
 class ProcessSubmission implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
 
-    public function __construct(private Submission $submission)
+    public function __construct(private array  $submissionData)
     {
     }
 
     public function handle(): void
     {
-        $this->submission->save();
+        Submission::create($this->submissionData);
     }
 }
